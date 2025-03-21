6018312d084255597910f9e7e9b5b3ca74fe63ed
Hastings <hastingslhx@gmail.com>
2025-03-21 08:46:41 +0800

    编码方式修改之后的提交

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,11 +1,11 @@
-锘縤mport edu.princeton.cs.algs4.*;
 
+import edu.princeton.cs.algs4.*;
 public class NBody {
 	public static double readRadius(String path){
-		// 返回其内部特定的宇宙的半径信息;
+
 		In in=new In(path);
 		if(in==null){
-			System.out.println("in 创建失败");return -1.0;
+			System.out.println("in 鏂囦欢鎵撳紑澶辫触");return -1.0;
 		}
 		int N=in.readInt();
 		double radius=in.readDouble();
@@ -14,7 +14,7 @@ public class NBody {
 	public static Planet[] readPlanets(String path){
 		In in=new In(path);
 		if(in==null){
-            System.out.println("in 创建失败");return null;
+            System.out.println("in 鏂囦欢鎵撳紑澶辫触");return null;
         }
 		int N=in.readInt();
 		double radius=in.readDouble();
@@ -32,14 +32,13 @@ public class NBody {
 	}
 
 	public static void draw(Planet planet){
-		// 展示出来一个星球的信息是这样的;
+
 		StdDraw.picture(planet.xxPos,planet.yyPos, "images/"+planet.imgFileName);
 		return;
 	}
 
 	public static void main(String []args){
-		// 设置第一个参数为确定的时间位置；
-		// 后面一个参数为相关的时间间隔信息;
+
 		double T=Double.parseDouble(args[0]);
         double dt=Double.parseDouble(args[1]);
 		In in=new In(args[2]);
@@ -56,13 +55,13 @@ public class NBody {
 			planets[i]=new Planet(xPos,yPos,xVel,yVel,mass,img);
 
 		}
-		// 内容的初始化操作;
+
 		double t=0;
 		double [] xForces=new double[N];
 		double [] yForces=new double[N];
-		// 每个行星在xy 两个方向上的总的受力数据;
+
 		StdDraw.enableDoubleBuffering();
-		// 设置绘图区域的尺度为圆形区域，范围从 -radius 到 +radius
+
 		StdDraw.setScale(-radius, radius);
 		// StdDraw.picture(0,0,"images/starfield.jpg");
 		while(t<T){
@@ -73,12 +72,12 @@ public class NBody {
 			for(int i=0;i<N;i++ ){
 				planets[i].update(dt, xForces[i], yForces[i]);
 			}
-			//绘制整体的图像;
+
 			StdDraw.clear();
 			StdDraw.picture(0,0,"images/starfield.jpg");
 
 			for(int i=0;i<N;i++){
-				// StdDraw.picture(planets[i].xxPos,planets[i].yyPos, "images/"+planets[i].imgFileName);
+
 				draw(planets[i]);
 			}
 			StdDraw.show();
@@ -97,5 +96,4 @@ public class NBody {
 
 	}
 }
-// 也就是全都需要转换为UTF-8 的形式进行编码是吗;
 
