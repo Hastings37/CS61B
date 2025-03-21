620c758617ca178e546795015803697e828ed44e
Hastings <hastingslhx@gmail.com>
2025-03-21 08:46:47 +0800

    编码方式修改之后的提交

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -12,13 +12,13 @@ public class Planet {
         this(0, 0, 0, 0, 1, "default.gif");
     }
 	public Planet(Planet p){
-		// 创建出来一个新的类型的指定的对象是这样的;
+		// 锟斤拷锟斤拷锟斤拷锟斤拷一锟斤拷锟铰碉拷锟斤拷锟酵碉拷指锟斤拷锟侥讹拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷;
 		this.xxPos=p.xxPos;
 		this.yyPos=p.yyPos;
 		this.xxVel=p.xxVel;
 		this.yyVel=p.yyVel;
 		this.mass=p.mass;
-		this.imgFileName=p.imgFileName;  // 对象的属性与参数一样, 所以直接赋值; 对象的属性与参数不一样, 所以要复制.  对象的属性和参数是对象的状态, 所以可以直接复制.   对象的属性和参数是对象的状态, 所以可以直接复制.   对象的属性和参数是对象的状态, 所以可以直接复制.   对象的属性和参数是对象的状态, 所以可以直接
+		this.imgFileName=p.imgFileName;  // 锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷一锟斤拷, 锟斤拷锟斤拷直锟接革拷值; 锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷一锟斤拷, 锟斤拷锟斤拷要锟斤拷锟斤拷.  锟斤拷锟斤拷锟斤拷锟斤拷院筒锟斤拷锟斤拷嵌锟斤拷锟斤拷状态, 锟斤拷锟皆匡拷锟斤拷直锟接革拷锟斤拷.   锟斤拷锟斤拷锟斤拷锟斤拷院筒锟斤拷锟斤拷嵌锟斤拷锟斤拷状态, 锟斤拷锟皆匡拷锟斤拷直锟接革拷锟斤拷.   锟斤拷锟斤拷锟斤拷锟斤拷院筒锟斤拷锟斤拷嵌锟斤拷锟斤拷状态, 锟斤拷锟皆匡拷锟斤拷直锟接革拷锟斤拷.   锟斤拷锟斤拷锟斤拷锟斤拷院筒锟斤拷锟斤拷嵌锟斤拷锟斤拷状态, 锟斤拷锟皆匡拷锟斤拷直锟斤拷
 	}
 	public Planet(double xxPos, double yyPos, double xxVel, double yyVel, double mass, String imgFileName) {
 		this.xxPos = xxPos;
@@ -29,16 +29,15 @@ public class Planet {
 		this.imgFileName = imgFileName;
 	}
 
-	// 计算两个星球之间的距离
 	public double calcDistance(Planet other) {
         double dx = this.xxPos - other.xxPos;
         double dy = this.yyPos - other.yyPos;
         return Math.sqrt(dx * dx + dy * dy);
-    }// 这真的很好了
+    }
 
-	// 计算两个星球之间的引力大小（数值信息） 恒定是一个正数的状态;
+
 	public double calcForceExertedBy(Planet other) {
-		// 计算的是当前的行星收到这个行星的引力的大小状态;
+
 		double dr=calcDistance(other);
 		double G=6.67430e-11;
 		return (G*mass*other.mass/(dr*dr));
@@ -57,12 +56,9 @@ public class Planet {
 		return Objects.hash(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);
 	}
 
-	// 指定的行星对我们的在X轴上面的影响效果;
+	// 鐜板湪鐨勫簲璇ユ槸姝ｅ父鐨勶紱
 	public double calcForceExertedByX(Planet other){
-		/* 根据其x位置的大小信息进行的判断要是位置是小的就是整数 反之就是复数
-		*  先是要搞明白这个是别的星球对我的作用的万有引力还是我对别的星球施加的万有引力状态
-		* */
-		// 计算的对象是传入的行星对我们当前的对象的影响的力的作用大小;
+
 		if(equals(other))return 0.0;
 		double dr=calcDistance(other);
 		double dx=other.xxPos-xxPos;
@@ -70,7 +66,7 @@ public class Planet {
 		double force=calcForceExertedBy(other);
 		return (force*dx/dr);
 	}
-	// 指定的行星对我们的在Y轴上面的影响效果;
+
 	public double calcForceExertedByY(Planet other){
 		if(equals(other))return 0.0;
 		double dr=calcDistance(other);
@@ -108,13 +104,9 @@ public class Planet {
 	//FAIL: calcNetForceExertedByX(): Expected 133.4 and you gave 133.49
 
 	public double moveDistance(double dt,double da){
-		// 随着指定的时间内和特定的加速度下面其走过的距离情况;
 		return 0.0;
 	}
 	public void update(double dt,double fx,double fy){
-		// 指定的时间内速度和位置的变化情况;
-		// double dx=(xxVel*dt+0.5*(fx/mass)*dt*dt);
-		// double dy=(yyVel*dt+0.5*(fy/mass)*dt*dt);
 		xxVel=xxVel+(fx/mass)*dt;
 		yyVel=yyVel+(fy/mass)*dt;
 		double dx=(xxVel*dt);
